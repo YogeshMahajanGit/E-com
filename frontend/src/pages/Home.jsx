633d24cd8def86a9abe377218a13f431e82818f1
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-02-19 12:10:06 -0800

    hero page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function Home() {
-  return <div className="bg-red-300">Home Tou</div>;
+  return (
+    <div className="">
+      <Hero />
+    </div>
+  );
 }
